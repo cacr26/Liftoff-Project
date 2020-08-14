9b6e018332f56ae3001db692e7ea6ebde5805eba
carlcitchen <carl.a.citchen@gmail.com>
2020-08-14 13:38:43 -0500

    "Initial Commit"

---
+++ src/main/java/org/liftoff/billtracker/BillTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.liftoff.billtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/liftoff/billtracker/BillTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liftoff.billtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
